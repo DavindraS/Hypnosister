12dc3f81645b3b4fded1f891ffeedd8263e0b340
Davindra Sutraban <razrsharp25@gmail.com>
2013-09-19 17:38:49 -0400

    Initial Commit

---
+++ Hypnosister/HypnosisterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnosisterAppDelegate.h
+//  Hypnosister
+//
+//  Created by Davindra Sutraban on 9/19/13.
+//  Copyright (c) 2013 Davindra Sutraban. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hypnosister/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hypnosister
+//
+//  Created by Davindra Sutraban on 9/19/13.
+//  Copyright (c) 2013 Davindra Sutraban. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HypnosisterAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HypnosisterAppDelegate class]));
+    }
+}
