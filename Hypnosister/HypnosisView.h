541cf32c370912dbf0e6aba220bbf9d4d3463d28
Davindra Sutraban <razrsharp25@gmail.com>
2013-09-19 17:52:20 -0400

    Added custom view

---
+++ Hypnosister/HypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  HypnosisView.h
+//  Hypnosister
+//
+//  Created by Davindra Sutraban on 9/19/13.
+//  Copyright (c) 2013 Davindra Sutraban. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HypnosisView : NSObject
+
+@end
---
+++ Hypnosister/HypnosisView.m
@@ -0,0 +1,13 @@
+//
+//  HypnosisView.m
+//  Hypnosister
+//
+//  Created by Davindra Sutraban on 9/19/13.
+//  Copyright (c) 2013 Davindra Sutraban. All rights reserved.
+//
+
+#import "HypnosisView.h"
+
+@implementation HypnosisView
+
+@end
