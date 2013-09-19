770ff9eb19e11d82a6900d6ce244904c309ecb5c
Davindra Sutraban <razrsharp25@gmail.com>
2013-09-19 18:17:48 -0400

    Created a drawing context and set bounds rectangle for current view

--- Hypnosister/HypnosisView.m
+++ Hypnosister/HypnosisView.m
@@ -10,4 +10,17 @@
 
 @implementation HypnosisView
 
+- (void)drawRect:(CGRect)rect
+{
+    // Set a context (which maintains the state of the drawing ie. color/thickness of pen) to the current context
+    // that is automatically created
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    // bounds gives the view's size independent of the superviews size
+    // We don't care about the superview until we are ready to draw the view onto the screen
+    CGRect bounds = [self bounds];
+    
+    // Draw a circle in the center of the bounds rectangle (size of the current view)
+    
+}
+
 @end
