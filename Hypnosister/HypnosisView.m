093981aa21cc5c0b5fe9b1058b8b0b65c87c155b
Davindra Sutraban <razrsharp25@gmail.com>
2013-09-20 10:28:24 -0400

    Drawing text on the screen

--- Hypnosister/HypnosisView.m
+++ Hypnosister/HypnosisView.m
@@ -32,7 +32,7 @@
     CGContextSetLineWidth(ctx, 10);
     
     // Set the color of the line to be gray
-    CGContextSetRGBFillColor(ctx, .6, .6, .6, 1);
+    [[UIColor lightGrayColor] setStroke];
     
     // Draw concentric circles
     for (float currentRadius = maxRadius; currentRadius > 0; currentRadius -= 20) {
@@ -41,6 +41,41 @@
         // DRAW
         CGContextStrokePath(ctx);
     }
+    
+    // Create a string
+    NSString *text = @"You are getting sleepy.";
+    
+    // Get a font to draw it in
+    UIFont *font = [UIFont boldSystemFontOfSize:28];
+    
+    CGRect textRect;
+    
+    // sizeWithFont is deprecated - Docs recommend sizeWithAttributes
+    // This takes an NSDictionary of attribtutes
+    NSDictionary *attr = [NSDictionary dictionaryWithObject:font forKey:@"font"];
+    
+    // Set the size of the textRect the size of the string
+    textRect.size = [text sizeWithAttributes:attr];
+    
+    // Put string in the center of the rect
+    textRect.origin.x = center.x - textRect.size.width / 2.0;
+    textRect.origin.y = center.y - textRect.size.height / 2.0;
+    
+    // Set the fill color of the current context to black
+    [[UIColor blackColor] setFill];
+    /*
+    // Shadow that will move 4pts to the right and 3 pts down from the text
+    CGSize offset = CGSizeMake(4, 3);
+    
+    // Color of the shadow
+    CGColorRef color = [[UIColor darkGrayColor] CGColor];
+    
+    // Set the shadow
+    CGContextSetShadowWithColor(ctx, offset, 2.0, color);
+    */
+    // Draw the string
+    [text drawInRect:textRect withAttributes:attr];
+    
 }
 
 // Override default initializer to make the background clear
